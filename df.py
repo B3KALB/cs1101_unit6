4eca3db8e92b9f17e7d6b1f8af90b8ee7a148104
B3KALB <B3KALB@gmail.com>
2022-10-10 15:43:29 -0500

    cs1101_unit6
    df code

--- df.py
+++ df.py
@@ -1,27 +1,28 @@
 
 # Welcome to the Unit 6 Discussion Forum!
 
-
 # Use the terms "equivalent" and "identical" to distinguish between objects and values. 
-equivalent = '=='
-identical = 'is'
-
 # Illustrate the difference further using your own examples with Python lists and the “is” operator.
 # is operator
 
-a = [81, 82, 83]
-b = [81, 82, 83]
-	
-print(a == b)
-print(a is b)
+a = [1, 2, 3]
+b = [1, 2, 3]
+equivalent = a == b
+identical = a is b
+
+print(equivalent)
+print(identical)
 	
 b = a
-print(a == b)
-print(a is b)
+equivalent = a == b
+identical = a is b
+
+print(equivalent)
+print(identical)
 	
 # Using your own Python list examples, explain how objects, references, and aliasing relate to one another.
 
-b[0] = 5
+b[0] = 10
 print(a)
 
 # Finally, create your own example of a function that modifies a list passed in as an argument. 
